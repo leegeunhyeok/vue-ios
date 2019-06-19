3748dc8077e2e0b74f6e83dc792643758db6e8f3
leegeunhyeok <lghlove0509@naver.com>
2019-06-19 22:39:44 +0900

    :construction_worker: Add alias @

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,6 +1,7 @@
 import commonjs from 'rollup-plugin-commonjs'
 import vue from 'rollup-plugin-vue'
 import buble from 'rollup-plugin-buble'
+import alias from 'rollup-plugin-alias'
 
 export default {
   input: 'src/entry.js',
@@ -8,6 +9,9 @@ export default {
     name: 'VueiOS'
   },
   plugins: [
+    alias({
+      '@': './src'
+    }),
     commonjs(),
     vue({
       css: true,
