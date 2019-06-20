3fee2931ef5b5fc9f5060249b1d996b72b41b960
leegeunhyeok <lghlove0509@naver.com>
2019-06-21 00:26:13 +0900

    :sparkles: Add iView.vue

---
+++ src/components/iView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="i-view">
+    <slot/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'iView'
+}
+
+</script>
+
+<style lang="scss">
+.i-view {
+  width: 100%;
+  height: 100%;
+  padding: 1rem;
+}
+</style>
