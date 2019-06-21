ccd01194e0d60ebba895797f7a4d94b1ac541c08
leegeunhyeok <lghlove0509@naver.com>
2019-06-21 18:19:00 +0900

    :lipstick: Add font-size to navigation title text

--- src/components/iNavigationBar.vue
+++ src/components/iNavigationBar.vue
@@ -141,12 +141,14 @@ export default {
     }
 
     .i-navigation-bar--title {
-      color: #000;
       position: absolute;
       top: 0;
       left: 0;
       width: 100%;
       height: 100%;
+      color: #000;
+      font-size: .9rem;
+      font-weight: bold;
     }
   }
 }
