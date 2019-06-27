bbcfb8e820f9d2aea695b51ad6e94f03a5c9ad8f
leegeunhyeok <lghlove0509@naver.com>
2019-06-27 17:42:24 +0900

    :sparkles: Add iLabel.vue

---
+++ src/components/iLabel.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="i-label">
+    <slot/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'iLabel'
+}
+</script>
+
+<style lang="scss">
+@import '../common/style/common.scss';
+
+.i-label {
+  display: inline-block;
+  color: $light-primary-text-color;
+  font-size: 1rem;
+}
+</style>
