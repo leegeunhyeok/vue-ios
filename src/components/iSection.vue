0055b0d690d5f5c2f4b1b282c64d07160f7c3597
leegeunhyeok <lghlove0509@naver.com>
2019-06-27 13:40:16 +0900

    :sparkles: Add iSection.vue

---
+++ src/components/iSection.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="i-section">
+    <slot/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'iSection'
+}
+</script>
+
+<style lang="scss">
+@import '../common/style/common.scss';
+
+.i-section {
+  width: 100%;
+  padding: 0 1rem;
+  padding-top: 2rem;
+  background-color: $light-background-color;
+}
+</style>
