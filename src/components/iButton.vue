fe561dc6a613a2897070f2cf6e3494fd63345a61
leegeunhyeok <lghlove0509@naver.com>
2019-06-28 11:23:37 +0900

    :sparkles: Add iButton

---
+++ src/components/iButton.vue
@@ -0,0 +1,37 @@
+<template>
+  <button class="i-button">
+    <slot/>
+  </button>
+</template>
+
+<script>
+import button from '@/mixins/button'
+
+export default {
+  name: 'iButton',
+  mixins: [ button ]
+}
+</script>
+
+<style lang="scss">
+@import '../common/style/common.scss';
+
+.i-button {
+  cursor: pointer;
+  outline: none;
+  border: none;
+  color: $ios-blue-color;
+  background-color: transparent;
+  font-size: 1rem;
+  padding: .5rem 1rem;
+  -webkit-transition: $transition-speed;
+     -moz-transition: $transition-speed;
+      -ms-transition: $transition-speed;
+       -o-transition: $transition-speed;
+          transition: $transition-speed;
+
+  &:hover {
+    color: $ios-lightblue-color;
+  }
+}
+</style>
