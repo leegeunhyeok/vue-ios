ae62800554426f86469d69882ddce5ffcbfcd853
dev.ghlee@gmail.com <leegeunhyeok>
2019-07-06 14:33:18 +0900

    :construction: Import component for build

--- src/entry.js
+++ src/entry.js
@@ -2,6 +2,7 @@ import iAlert from './components/iAlert.vue'
 import iButton from './components/iButton.vue'
 import iLabel from './components/iLabel.vue'
 import iNavigationBar from './components/iNavigationBar.vue'
+import iSearchField from './components/iSearchField.vue'
 import iSwitch from './components/iSwitch.vue'
 import iTable from './components/iTable.vue'
 import iTableItem from './components/iTableItem.vue'
@@ -13,6 +14,7 @@ const components = {
   iButton,
   iLabel,
   iNavigationBar,
+  iSearchField,
   iSwitch,
   iTable,
   iTableItem,
@@ -20,8 +22,11 @@ const components = {
   iView
 }
 
-export function install(Vue) {
-	if (install.installed) return
+export function install (Vue) {
+  if (install.installed) {
+    return
+  }
+
   install.installed = true
   Object.keys(components).forEach(name => {
     Vue.component(name, components[name])
@@ -29,18 +34,18 @@ export function install(Vue) {
 }
 
 const plugin = {
-	install
+  install
 }
 
 let GlobalVue = null
 if (typeof window !== 'undefined') {
-	GlobalVue = window.Vue
+  GlobalVue = window.Vue
 } else if (typeof global !== 'undefined') {
-	GlobalVue = global.Vue
+  GlobalVue = global.Vue
 }
 
 if (GlobalVue) {
-	GlobalVue.use(plugin)
+  GlobalVue.use(plugin)
 }
 
 export {
@@ -48,6 +53,7 @@ export {
   iButton,
   iLabel,
   iNavigationBar,
+  iSearchField,
   iSwitch,
   iTable,
   iTableItem,
