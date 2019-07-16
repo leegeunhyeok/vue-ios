6acb3d0d13a2f8e1af8f3769a9a14866e21ff090
leegeunhyeok <lghlove0509@naver.com>
2019-07-16 13:39:50 +0900

    :sparkles: Add iBoard

---
+++ src/components/iBoard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="i-board">
+    <slot/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'iBoard',
+}
+
+</script>
+
+<style lang="scss">
+.i-board {
+  width: 100%;
+}
+</style>
