901e36bbfc54ba4f675ab271f0e8072d34d727d8
leegeunhyeok <lghlove0509@naver.com>
2019-08-24 15:45:10 +0900

    :zap: Update new navigationbar!

--- src/components/iApp.vue
+++ src/components/iApp.vue
@@ -1,18 +1,31 @@
 <template>
   <div class="i-app">
     <div class="i-main">
-      <div class="i-header">
+      <div class="i-header"
+        :style="headerStyle"
+      >
         <div class="i-header__left">
           <slot name="headerLeft"/>
         </div>
-        <div class="i-header__title">
+        <div class="i-header__title"
+          :style="headerTitleOpacity"
+        >
           {{ title }}
         </div>
         <div class="i-header__right">
           <slot name="headerRight"/>
         </div>
       </div>
-      <div class="i-main__content">
+      <div class="i-main__content" ref="main">
+        <div class="i-main__large-header"
+          v-if="largeHeader"
+        >
+          <div class="i-main__large-header__title"
+            :style="largeHeaderPosition"
+          >
+            {{ largeTitle }}
+          </div>
+        </div>
         <iMainView ref="main">
           <slot name="main"/>
         </iMainView>
@@ -30,7 +43,7 @@
           <slot name="headerRight"/>
         </div>
       </div>
-      <div class="i-sub__content">
+      <div class="i-sub__content" ref="sub">
         <iSubView>
           <slot name="sub"/>
         </iSubView>
@@ -53,7 +66,15 @@ export default {
     header: {
       type: Boolean
     },
+    largeHeader: {
+      type: Boolean,
+      default: true
+    },
     title: {
+      type: String,
+      default: 'Header'
+    },
+    largeTitle: {
       type: String,
       default: 'Header'
     },
@@ -64,13 +85,26 @@ export default {
   },
   data () {
     return {
-      subNavigationTitle: 'Sub view',
-      showSubTitle: false
+      rootEmPx: 12,
+      largeHeaderPositionPx: 0,
+      opacity: 1
     }
   },
-  watch: {
-    $iWidth (newVal) {
-      this.showSubTitle = newVal >= 700
+  computed: {
+    headerStyle () {
+      return {
+        'border-color': `rgba(197, 197, 200, ${this.opacity})`
+      }
+    },
+    headerTitleOpacity () {
+      return {
+        'opacity': this.opacity
+      }
+    },
+    largeHeaderPosition () {
+      return {
+        'top': this.largeHeaderPositionPx + 'px'
+      }
     }
   },
   components: {
@@ -78,13 +112,46 @@ export default {
     iSubView,
     iNavigationBar
   },
+  created () {
+    this.opacity = this.largeHeader ? 0 : 1
+  },
   mounted () {
-    window.addEventListener('resize', this.watchWindowSize)
+    this.getRem()
+    window.addEventListener('resize', this.getRem)
+    this.$refs.main.addEventListener('scroll', this.updateScrollUI)
+    this.$refs.sub.addEventListener('scroll', this.updateScrollUI)
   },
   beforeDestroy () {
-    window.removeEventListener('resize', this.watchWindowSize)
+    window.removeEventListener('resize', this.getRem)
+    this.$refs.main.removeEventListener('scroll', this.updateScrollUI)
+    this.$refs.sub.removeEventListener('scroll', this.updateScrollUI)
   },
   methods: {
+    getRem () {
+      this.rootEmPx = parseFloat(
+        getComputedStyle(document.body)
+          .getPropertyValue('font-size')
+      )
+    },
+    updateScrollUI (ev) {
+      if (!ev) return 1
+
+      if (!this.largeHeader) {
+        return 1
+      }
+      
+      let opacity = 1
+      const scrollTop = ev.target.scrollTop
+      if (this.rootEmPx * 4 >= scrollTop) {
+        this.largeHeaderPositionPx = scrollTop
+        opacity = 0
+      } else {
+        this.largeHeaderPositionPx = this.rootEmPx * 4
+        opacity = (scrollTop - this.rootEmPx * 3) / this.rootEmPx * 3 * 0.1
+        opacity = Math.min(opacity, 1)
+      }
+      this.opacity = opacity
+    },
     watchWindowSize () {
       this.$iWidth = event.target.innerWidth
     }
@@ -145,6 +212,23 @@ html, body, .i-app {
 
       @media only screen and (min-width: 768px), (min-width: 1224px) {
         padding-top: 3rem;
+      }
+    }
+
+    &__large-header {
+      position: relative;
+      border-bottom: 1px solid;
+      border-color: $light-border-color;
+      background-color: $light-background-color;
+      height: 6rem;
+
+      &__title {
+        position: absolute;
+        width: 100%;
+        padding: .25rem 1rem;
+        background-color: $light-background-color;
+        font-weight: bold;
+        font-size: 2rem;
       }
     }
   }
